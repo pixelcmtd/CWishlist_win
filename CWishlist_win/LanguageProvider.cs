283e500fdb4ccb49c861f4fdbf2f7000f93679bc
chrissx <christian.chrissicx2004@freenet.de>
2018-06-15 20:33:36 +0200

    Shortening default and removing checking NodeType twice.

--- CWishlist_win/LanguageProvider.cs
+++ CWishlist_win/LanguageProvider.cs
@@ -15,48 +15,49 @@ namespace CWishlist_win
 
         public static void load_lang_xml(string file)
         {
-            lang lng = default(lang);
+            lang lng = default;
             Dictionary<string, dynamic> translations = new Dictionary<string, dynamic>();
             XmlReader xml = XmlReader.Create(file);
             while (xml.Read())
-                if (xml.NodeType == XmlNodeType.Element && xml.Name == "lang")
-                    lng = new lang(xml.GetAttribute("code"), xml.GetAttribute("name"));
-                else if (xml.NodeType == XmlNodeType.Element && xml.Name == "translation")
-                {
-                    string name = xml.GetAttribute("name");
-                    string type = xml.GetAttribute("type");
-                    string val = xml.GetAttribute("value");
-                    dynamic value = -1;
-                    switch (type)
+                if(xml.NodeType == XmlNodeType.Element)
+                    if (xml.Name == "lang")
+                        lng = new lang(xml.GetAttribute("code"), xml.GetAttribute("name"));
+                    else if (xml.Name == "translation")
                     {
-                        case "str_arr": value = val.Split('\\'); break;
-                        case "str": value = val; break;
-                        case "int_arr": value = val.Split('\\').parse_ints(); break;
-                        case "int": value = int.Parse(val); break;
-                        case "uint_arr": value = val.Split('\\').parse_uints(); break;
-                        case "uint": value = uint.Parse(val); break;
-                        case "short_arr": value = val.Split('\\').parse_shorts(); break;
-                        case "short": value = short.Parse(val); break;
-                        case "ushort_arr": value = val.Split('\\').parse_ushorts(); break;
-                        case "ushort": value = ushort.Parse(val); break;
-                        case "long_arr": value = val.Split('\\').parse_longs(); break;
-                        case "long": value = long.Parse(val); break;
-                        case "ulong_arr": value = val.Split('\\').parse_ulongs(); break;
-                        case "ulong": value = ulong.Parse(val); break;
-                        case "byte_arr": value = val.Split('\\').parse_bytes(); break;
-                        case "byte": value = byte.Parse(val); break;
-                        case "sbyte_arr": value = val.Split('\\').parse_sbytes(); break;
-                        case "sbyte": value = sbyte.Parse(val); break;
-                        case "decimal_arr": value = val.Split('\\').parse_decimals(); break;
-                        case "decimal": value = decimal.Parse(val); break;
-                        case "bool_arr": value = val.Split('\\').parse_bools(); break;
-                        case "bool": value = bool.Parse(val); break;
-                        case "char_arr": value = val.Split('\\').parse_chars(); break;
-                        case "char": value = char.Parse(val); break;
-                        default: value = -1; break;
+                        string name = xml.GetAttribute("name");
+                        string type = xml.GetAttribute("type");
+                        string val = xml.GetAttribute("value");
+                        dynamic value = -1;
+                        switch (type)
+                        {
+                            case "str_arr": value = val.Split('\\'); break;
+                            case "str": value = val; break;
+                            case "int_arr": value = val.Split('\\').parse_ints(); break;
+                            case "int": value = int.Parse(val); break;
+                            case "uint_arr": value = val.Split('\\').parse_uints(); break;
+                            case "uint": value = uint.Parse(val); break;
+                            case "short_arr": value = val.Split('\\').parse_shorts(); break;
+                            case "short": value = short.Parse(val); break;
+                            case "ushort_arr": value = val.Split('\\').parse_ushorts(); break;
+                            case "ushort": value = ushort.Parse(val); break;
+                            case "long_arr": value = val.Split('\\').parse_longs(); break;
+                            case "long": value = long.Parse(val); break;
+                            case "ulong_arr": value = val.Split('\\').parse_ulongs(); break;
+                            case "ulong": value = ulong.Parse(val); break;
+                            case "byte_arr": value = val.Split('\\').parse_bytes(); break;
+                            case "byte": value = byte.Parse(val); break;
+                            case "sbyte_arr": value = val.Split('\\').parse_sbytes(); break;
+                            case "sbyte": value = sbyte.Parse(val); break;
+                            case "decimal_arr": value = val.Split('\\').parse_decimals(); break;
+                            case "decimal": value = decimal.Parse(val); break;
+                            case "bool_arr": value = val.Split('\\').parse_bools(); break;
+                            case "bool": value = bool.Parse(val); break;
+                            case "char_arr": value = val.Split('\\').parse_chars(); break;
+                            case "char": value = char.Parse(val); break;
+                            default: value = -1; break;
+                        }
+                        translations.Add(name, value);
                     }
-                    translations.Add(name, value);
-                }
             langs.Add(lng, translations);
         }
     }
