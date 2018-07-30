c4fffcfb5678ace2ce33001671726f18de44075f
chrissx <christian.chrissicx2004@freenet.de>
2018-07-30 17:53:27 +0200

    Built 6.0.0

--- CWishlist_win/_BUILD/build.bat
+++ CWishlist_win/_BUILD/build.bat
@@ -63,12 +63,12 @@ echo ^<--------------------^>
 echo Copying to output dir.
 echo ^<--------------------^>
 echo.
-echo copy AnyCpu\\CWishlist_win.exe binaries\\CWishlist_win%version%AnyCpu.exe
-copy AnyCpu\\CWishlist_win.exe binaries\\CWishlist_win%version%AnyCpu.exe
-echo copy x86\\CWishlist_win.exe binaries\\CWishlist_win%version%x86.exe
-copy x86\\CWishlist_win.exe binaries\\CWishlist_win%version%x86.exe
-echo copy x64\\CWishlist_win.exe binaries\\CWishlist_win%version%x64.exe
-copy x64\\CWishlist_win.exe binaries\\CWishlist_win%version%x64.exe
+echo copy AnyCpu\\CWishlist_win.exe binaries\\CWishlist_win_%version%_AnyCpu.exe
+copy AnyCpu\\CWishlist_win.exe binaries\\CWishlist_win_%version%_AnyCpu.exe
+echo copy x86\\CWishlist_win.exe binaries\\CWishlist_win_%version%_x86.exe
+copy x86\\CWishlist_win.exe binaries\\CWishlist_win_%version%_x86.exe
+echo copy x64\\CWishlist_win.exe binaries\\CWishlist_win_%version%_x64.exe
+copy x64\\CWishlist_win.exe binaries\\CWishlist_win_%version%_x64.exe
 echo.
 echo ^<-------------------^>
 echo Copied to output dir.
